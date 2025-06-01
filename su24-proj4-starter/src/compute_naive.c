ec9de95f75841940e10faa040fc5bfb1e3100265
kenqia <2696015553@qq.com>
2025-06-01 14:05:45 +0800

    proj4

---
+++ su24-proj4-starter/src/compute_naive.c
@@ -0,0 +1,83 @@
+#include "compute.h"
+
+
+int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
+  int a_rows = a_matrix->rows;
+  int a_cols = a_matrix->cols;
+  int b_rows = b_matrix->rows;
+  int b_cols = b_matrix->cols;
+  int32_t *tmp_data = malloc(b_cols * b_rows * sizeof(int32_t));
+  if(tmp_data == NULL) return -1;
+  for(int i = 0 ; i < b_rows; i++){
+    for(int j = 0 ; j < b_cols ; j++){
+      tmp_data[i * b_cols + j]= b_matrix->data[(b_rows - i - 1) * b_cols + (b_cols - 1 - j)]; 
+    }
+  }
+  matrix_t *rslt_matrix = malloc(sizeof(matrix_t));
+  if(rslt_matrix == NULL){
+    free(tmp_data);
+    free(rslt_matrix);  
+    return -1;
+  }
+  rslt_matrix->rows = a_rows - b_rows + 1;
+  rslt_matrix->cols = a_cols - b_cols + 1;
+  rslt_matrix->data = malloc(rslt_matrix->rows * rslt_matrix->cols * sizeof(int32_t));
+  int down, right;
+  down = right = 0;
+  while(down < rslt_matrix->rows && right < rslt_matrix->cols){
+    int sum = 0;
+    for(int i = 0 ; i < b_rows ; i++){
+      for(int j = 0 ; j < b_cols ; j++){
+        sum += a_matrix->data[(i + down)* a_cols + j + right] * tmp_data[i * b_cols + j];
+      }
+    }
+    rslt_matrix->data[down * rslt_matrix->cols + right] = sum;
+    right++;
+    if(right >= rslt_matrix->cols){
+      right = 0;
+      down++;
+    }
+  }
+  *output_matrix = rslt_matrix;
+  free(tmp_data);
+  return 0;
+}
+
+// Executes a task
+int execute_task(task_t *task) {
+  matrix_t *a_matrix, *b_matrix, *output_matrix;
+
+  char *a_matrix_path = get_a_matrix_path(task);
+  if (read_matrix(a_matrix_path, &a_matrix)) {
+    printf("Error reading matrix from %s\n", a_matrix_path);
+    return -1;
+  }
+  free(a_matrix_path);
+
+  char *b_matrix_path = get_b_matrix_path(task);
+  if (read_matrix(b_matrix_path, &b_matrix)) {
+    printf("Error reading matrix from %s\n", b_matrix_path);
+    return -1;
+  }
+  free(b_matrix_path);
+
+  if (convolve(a_matrix, b_matrix, &output_matrix)) {
+    printf("convolve returned a non-zero integer\n");
+    return -1;
+  }
+
+  char *output_matrix_path = get_output_matrix_path(task);
+  if (write_matrix(output_matrix_path, output_matrix)) {
+    printf("Error writing matrix to %s\n", output_matrix_path);
+    return -1;
+  }
+  free(output_matrix_path);
+
+  free(a_matrix->data);
+  free(b_matrix->data);
+  free(output_matrix->data);
+  free(a_matrix);
+  free(b_matrix);
+  free(output_matrix);
+  return 0;
+}
